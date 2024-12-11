ef545f411a8d8d6c2375bb6e76860215cc516532
RishabChoudhary75 <rishab.choudhary.hp@gmail.com>
2024-12-11 18:42:31 +0530

    new code

--- loop.js
+++ loop.js
@@ -1,7 +1,8 @@
-let a = [10, 70, -39, 77, -45];
+let a = [10, 70, -39, 7, -45];
 let sum = 0;
 for (let i = 0; i < a.length; i++) {
   if (a[i] > 0) {
+    console.log(a[i])
     sum = sum + a[i];
   }
 } 
